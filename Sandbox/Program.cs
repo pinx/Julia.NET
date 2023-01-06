e69e9b36ea2f0137da85a449d1eaa6a840e72409
Johnathan Bizzano <69430085+HyperSphereStudio@users.noreply.github.com>
2023-01-06 12:19:48 -0700

    Update Program.cs

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -9,19 +9,13 @@ try
     // jo.LoadSystemImage = "my_sys_image_path";
     Julia.Init(jo);
 
-    JModule myModule = Julia.Eval(@"
-                    module T
-                        add!(m1, m2) = m1 .+= m2
-                    end
-                    using Main.T
-                    return T");
+    Julia.Eval("include(\"test.jl\")");
 
-    var m1 = new[] { 2, 3, 4 };
-    var m2 = new[] { 3, 4, 5 };
-
-    myModule.GetFunction("add!").Invoke(new Any(m1), new Any(m2));
-    string.Join(",", m1).Println();
-
+    var topMod = Julia.Eval("TopMod");
+    var subMod = topMod.GetGlobal("SubMod");
+    var subF = subMod.GetFunction("SubF");
+    var topF = topMod.GetFunction("TopF");
+    
     Julia.Exit();
 }
 catch (Exception e)
